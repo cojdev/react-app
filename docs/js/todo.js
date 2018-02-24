bf0609f9e62fed33405c27aef21a54fe8e772e00
Charles Ojukwu <charles.ojukwu1@gmail.com>
2018-02-24 19:57:00 +0000

    Todo App functionality added

--- docs/js/todo.js
+++ docs/js/todo.js
@@ -21,18 +21,40 @@ function uuid(len) {
   return string;
 }
 
-class TaskForm extends React.Component {
-	addTask(e) {
+class AddTask extends React.Component {
+
+	constructor() {
+		super();
+		this.state = {
+			newTask: {}
+		};
+	}
+	
+	handleSubmit(e) {
+		if (this.refs.taskName.value === '') {
+			alert('Please enter a task');
+		}
+		else {
+			this.setState({newTask: {
+				content: this.refs.taskName.value,
+				completed: false,
+				id: uuid()
+			}}, function () {
+				console.log(this.state);
+				this.props.addTask(this.state.newTask);
+				this.refs.taskName.value = '';
+			});
+		}
 		e.preventDefault();
 	}
 
   render() {
     return (
-      <form onSubmit={this.addTask.bind(this)} className="task-form">
+      <form onSubmit={this.handleSubmit.bind(this)} className="task-form">
 				<div className="task-input">
-					<input type="text" placeholder="What do you need to do?"/>
+					<input type="text" ref="taskName" placeholder="What do you need to do?"/>
 				</div>
-				<button className="task-add-button" type="Submit">
+				<button className="task-add-button" type="submit" value="Submit">
 					<svg viewBox="0 0 40 40">
 						<path d="M10 20 L30 20 M20 10 L20 30" />
 					</svg>
@@ -42,19 +64,46 @@ class TaskForm extends React.Component {
   }
 }
 
+class TaskItem extends React.Component {
+	removeTask(id) {
+		this.props.onRemove(id);
+	}
+
+	checkTask(id) {
+		this.props.onCheck(id);
+	}
+
+	render() {
+		return (
+			<li>
+				<input
+					id={this.props.task.id}
+					type="checkbox"
+					checked={this.props.task.completed}
+					onChange={this.checkTask.bind(this, this.props.task.id)}/>
+				<label htmlFor={this.props.task.id}>
+					{this.props.task.content}
+				</label>
+				<button
+					className="task-item-remove"
+					onClick={this.removeTask.bind(this, this.props.task.id)}>
+					<svg viewBox="0 0 40 40">
+						<path d="M15 15 L25 25 M25 15 L15 25" />
+					</svg>
+				</button>
+			</li>
+		);
+		
+	}
+}
+
 class TaskList extends React.Component {
+
 	render() {
 		let taskItems = this.props.tasks.map(task => {
       console.log(task.id);
       return (
-				<li key={task.id}><input id={task.id} type="checkbox"/>
-          <label htmlFor={task.id}>{task.content}</label>
-          <button className="task-item-remove">
-						<svg viewBox="0 0 40 40">
-							<path d="M15 15 L25 25 M25 15 L15 25" />
-						</svg>
-					</button>
-        </li>
+				<TaskItem task={task} key={task.id} onRemove={this.props.removeTask.bind(this)} onCheck={this.props.checkTask.bind(this)} />
       )
     });
 
@@ -70,25 +119,57 @@ class App extends React.Component {
 	constructor() {
 		super();
 		this.state = {
-			tasks: [
-				{
-					id: uuid(),
-					content: "Make task list",
-					completed: false 
-				},
-				{
-					id: uuid(),
-					content: "Make Presentation",
-					completed: true
-				}
-			]
+			tasks: []
 		}
 	}
+
+	componentWillMount() {
+		this.setState({tasks: [
+			{
+				id: uuid(),
+				content: "Make task list",
+				completed: false 
+			},
+			{
+				id: uuid(),
+				content: "Make Presentation",
+				completed: true
+			}
+		]});
+
+	}
+
+	handleAddTask(task) {
+		console.log(task);
+		let tasks = this.state.tasks;
+		tasks.unshift(task);
+		this.setState({tasks: tasks});
+	}
+
+	handleRemoveTask(id) {
+		console.log('Delete ' + id);
+		let tasks = this.state.tasks;
+		let target = tasks.findIndex(index => index.id === id);
+		tasks.splice(target, 1);
+		this.setState({tasks: tasks});
+	}
+
+	handleCheckTask(id) {
+		console.log('Check ' + id);
+		let tasks = this.state.tasks;
+		let target = tasks.findIndex(index => index.id === id);
+		tasks[target].completed = tasks[target].completed === true ? false : true;
+		this.setState({tasks: tasks});
+	}
+
 	render() {
 		return (
 			<div className="app">
-        <TaskForm />
-				<TaskList tasks={this.state.tasks} />
+        <AddTask addTask={this.handleAddTask.bind(this)} />
+				<TaskList 
+					tasks={this.state.tasks}
+					removeTask={this.handleRemoveTask.bind(this)}
+					checkTask={this.handleCheckTask.bind(this)} />
 			</div>
 		);
 	}
